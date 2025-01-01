a218baa63c44d5fb2043c3bdc45060d5f01a8960
Anurag 729 <anuragran07@gmail.com>
2025-01-01 21:27:53 +0530

    added a controller for showing channels based on the search results

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -315,6 +315,79 @@ const updateCoverImg = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
+const selectUserChannel = asyncHandler(async (req, res) => {
+  const { userName } = req.params;
+
+  if (!userName.trim()) {
+    throw new ApiError(400, "Username not recieved");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: { userName: userName },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        subscribedCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        userName: 1,
+        fullName: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+        subscribersCount: 1,
+        subscribedCount: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channel[0],
+        "Channel details have been fetched successfully"
+      )
+    );
+});
+
 export {
   registerUser,
   userLogin,
